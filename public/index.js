4e5005b608ce8819b8e91018a2a94c662117ad22
annalanigan <annalanigan@outlook.com>
2018-02-04 09:28:01 +0000

    added a search box and listed how many results there are for a word search

--- public/index.js
+++ public/index.js
@@ -12,21 +12,28 @@ var requestComplete = function(){
   var jsonString = this.responseText;
   var movies = JSON.parse(jsonString);
   // console.log("what are my moveis?", movies.Search);
-  populatePage(movies.Search);
+  populatePage(movies);
 }
 
 var populatePage = function(movies){
   var location = document.querySelector('#movies-list');
+  var results = document.querySelector('#total-results');
+  results.innerText = 'Total Results: ' + movies.totalResults;
+  console.log(movies.totalResults);
 
-  movies.forEach(function(movie){
-    console.log("what is my movie?", movie);
+  movies.Search.forEach(function(movie){
+    // console.log("what is my movie?", movie);
     var li = document.createElement('li');
     var title = document.createElement('p');
     title.innerText = movie.Title;
     var year = document.createElement('p');
     year.innerText = movie.Year;
     var img = document.createElement('img');
-    img.src = movie.Poster;
+    if (movie.Poster === "N/A"){
+      img.src = 'https://hprcc.unl.edu/images/No-Image-Available.jpg'
+    } else {
+      img.src = movie.Poster;
+    }
     img.width = 200;
     // console.log(movie.Title);
     li.appendChild(title);
@@ -43,9 +50,9 @@ searchQuery = function(){
 var app = function(){
   var key = new ApiKey();
   // var search = searchQuery();
-  var search = 'godzilla';
-  var url = 'http://www.omdbapi.com/?s=' + search + '&apikey=' + key.getKey();
-  // console.log(url);
+  var search = 'star';
+  var url = 'http://www.omdbapi.com/?s=' + search + '&page=1&type=movie&apikey=' + key.getKey();
+  console.log(url);
   makeRequest(url, requestComplete);
 }
 
